4d9141988251853b8a78cc4443955972c50d2c2e
JayRodButray <jayrodbutray@gmail.com>
2023-07-05 12:27:00 -0700

    start adding repo code,consolelog stuff

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
-const fs = require('fs');
-const { default: inquirer } = require('inquirer');
 
-function writeToFile(fileName, data) {
+/*function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
     if (err) {
       console.error('Error writing to file:', err);
@@ -17,18 +13,68 @@ function writeToFile(fileName, data) {
   });
 }
 writeToFile('README.md', 'Your README file.');
+*/
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// Create a prompt
 inquirer
-.prompt([
+  .prompt([
     {
-        type: 'input',
-        message: 'What is your name',
-        name: 'name',
+        message: 'What is the title of your project',
+        name: 'title',
     },
     {
-        type: 'input'
-    }
-]);
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+        message: 'What is your GitHub profile name?',
+        name: 'GitHub',
+    },
+  ])
+  .then((answers) => {
+    // The user's input will be available in the 'answers' object
+    const name = answers.name;
+    console.log(answers);
+
+    const url = `https://api.github.com/users/${answers.GitHub}/repos`;
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((repos) => {
+        console.log(repos);
+
+        const repoList = repos.map((repo) => {
+            const {full_name, html_url} = repo;
+
+            return `<li><a href = ${html_url}>${full_name}</a></li>`;
+        });
+
+
+        const repoCode = `<ul>${repoList.join('')}</ul>`;
+
+        console.log(repoCode);
+
+        answers.repos = repoCode;
+
+        let htmlContents = fs.readFileSync('index.tmpl.html', 'utf-8');
+
+        for (const responseKey in answers){
+            const templateKey = "{{" + responseKey + "}}"
+            htmlContents = htmlContents.replaceAll(templateKey, answers[responseKey]);
+        }
+    
+        const myPortfolio = htmlContents;
+    
+        fs.writeFileSync('index.html', myPortfolio);
+        console.log('It is done');
+      });
+  });
+
+
 
 
 // TODO: Create a function to initialize app
