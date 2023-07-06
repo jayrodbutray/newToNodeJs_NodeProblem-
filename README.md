13b1d5aba5a2ab33e1a678d5e2cd0f3186f2994c
JayRodButray <jayrodbutray@gmail.com>
2023-07-05 19:06:33 -0700

    adding links to table of contents

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-README.md
+
+        # aly
+
+        ## Table of Contents
+        - [Description](#description)
+        - [Author](#author)
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [License](#license)
+        
+        ## Description
+super sexy
+
+## Author
+jared
+
+GitHub Profile: [jayrodbutray](https://github.com/jayrodbutray)
+
+## Installation
+<!-- Add installation instructions here -->
+
+## Usage
+<!-- Add usage instructions here -->
+
+## License
+<!-- Add license information here -->
--- index.js
+++ index.js
@@ -80,7 +80,39 @@ inquirer
     
         fs.writeFileSync('index.html', myPortfolio);
         console.log('It is done');
-        writeToFile('README.md');
+
+        const readmeContent = `
+        # ${answers.title}
+
+## Table of Contents
+- [Description](#description)
+- [Author](#author)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Description <a name="description"></a>
+${answers.description}
+
+## Author <a name="author"></a>
+${answers.name}
+
+GitHub Profile: [${answers.GitHub}](https://github.com/${answers.GitHub})
+
+## Installation <a name="installation"></a>
+<!-- Add installation instructions here -->
+
+## Usage <a name="usage"></a>
+<!-- Add usage instructions here -->
+
+## License <a name="license"></a>
+<!-- Add license information here -->
+`;
+
+        
+    // Write the README content to file
+ 
+        writeToFile(readmeContent);
       });
   })
 
